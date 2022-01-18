f2b3cd6ad5fb5ea5048ec5c572f60633c3537832
jhadley1922 <90360017+jhadley1922@users.noreply.github.com>
2022-01-17 20:36:12 -0700

    Moving to the MVC model for m3 assignment

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using M2Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M2Assignment.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator (GradeCalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/GradeCalculatorModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M2Assignment.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Range(0, 100,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Assignment { get; set; }
+        [Range(0, 100,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Group { get; set; }
+        [Range(0, 100,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Quiz { get; set; }
+        [Range(0, 100,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Exam { get; set; }
+        [Range(0, 100,
+        ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Intex { get; set; }
+    }
+}
